34bb9c3653f045146ea58895f2576cd34ac1e296
thuan20132000 <thuan20132000@gmail.com>
2020-12-22 18:28:21 +0700

    Added Statics in HomeAdmin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\JobCollection;
+use App\Model\Job;
+use App\Model\JobCollaborator;
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,7 +27,40 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+
+        $colaborator_total = User::getCollaborators()->toArray();
+        $customer_total = User::getCustomer()->toArray();
+        $jobs_total = Job::all()
+        ->groupBy('occupation_id');
+        // $a = JobCollection::collection($jobs_total);
+
+        $job_collaborators = JobCollaborator::where('status',JobCollaborator::CONFIRMED)
+        ->get();
+
+        $profit_sum_of_collaborator = 0;
+        $success_job_collaborator_number = count($job_collaborators);
+        foreach ($job_collaborators as $value) {
+            # code...
+            $profit_sum_of_collaborator = $profit_sum_of_collaborator + $value->confirmed_price;
+        }
+
+
+
+
+        $collaborators = User::hydrate($colaborator_total);
+        $customers = User::hydrate($customer_total);
+        // dd($jobs_total);
+
+        // dd(User::hydrate($colaborator_total));
+        // dd(count($colaborator_total));
+
+        return view('admin.index',[
+            'jobs_total'=>$jobs_total,
+            'customers'=>$customers,
+            'collaborator_total'=>$collaborators,
+            'profit_sum_of_collaborators'=>$profit_sum_of_collaborator,
+            'success_job_collaborator_number'=>$success_job_collaborator_number
+        ]);
     }
 
     public function checkout()
